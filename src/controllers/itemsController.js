7f6f6f5a2b6116da27ec42c22cf0ef59c6f1100a
pedrorfigueiredo <pedrorfig27@gmail.com>
2020-08-21 12:29:08 -0300

    working form and get products

--- src/controllers/itemsController.js
+++ src/controllers/itemsController.js
@@ -9,12 +9,12 @@ cloudinary.config({
 });
 
 module.exports = {
-  async getAll(req, res) {
+  async getAll(req, res, next) {
     Item.find({}, (err, items) => {
       if (err) {
         res.status(500).json(err);
       } else {
-        res.status(201).json(items);
+        res.status(200).json(items);
       }
     });
   },
@@ -23,50 +23,72 @@ module.exports = {
     Item.find({ category: req.params.categoryId }, (err, items) => {
       if (err) {
         res.status(500).json(err);
-        console.log(err);
       } else {
-        res.status(201).json(items);
-        console.log(items);
+        res.status(200).json(items);
       }
     });
   },
 
-  create(req, res) {
-    cloudinary.uploader.upload(
-      req.file.path,
-      { folder: "so-carnes-silva" },
-      async (err, result) => {
-        if (err) {
-          fs.unlinkSync(req.file.path, (err) => {
+  async create(req, res) {
+    if (req.file) {
+      await cloudinary.uploader.upload(
+        req.file.path,
+        { folder: "so-carnes-silva" },
+        async (err, result) => {
+          if (err) {
+            fs.unlinkSync(req.file.path, (err) => {
+              res.status(500).json(err);
+              return;
+            });
             res.status(500).json(err);
+            return;
+          }
+
+          const item = new Item({
+            name: req.body.name,
+            category: req.body.category,
+            description: req.body.description,
+            price: parseFloat(req.body.price),
+            image: {
+              id: result.public_id,
+              url: result.secure_url,
+            },
+            unit: req.body.unit,
+            optionsList: JSON.parse(req.body.optionsList),
           });
+
+          await item.save((err) => {
+            if (err) {
+              res.status(500).json(err);
+              return;
+            }
+            fs.unlinkSync(req.file.path, (err) => {
+              res.status(500).json(err);
+              return;
+            });
+            res.status(201).json(item);
+            return;
+          });
+        }
+      );
+    } else {
+      const item = new Item({
+        name: req.body.name,
+        category: req.body.category,
+        description: req.body.description,
+        price: parseFloat(req.body.price),
+        image: null,
+        unit: req.body.unit,
+        optionsList: JSON.parse(req.body.optionsList),
+      });
+
+      await item.save((err) => {
+        if (err) {
           res.status(500).json(err);
           return;
         }
-
-        const item = new Item({
-          name: req.body.name,
-          category: req.body.category,
-          description: req.body.description,
-          price: parseFloat(req.body.price),
-          image: {
-            id: result.public_id,
-            url: result.secure_url,
-          },
-          unit: req.body.unit,
-          optionsList: JSON.parse(req.body.optionsList),
-        });
-
-        await item.save((err) => {
-          if (err) {
-            res.status(500).json(err);
-          }
-          fs.unlinkSync(req.file.path, (err) => {
-            res.status(500).json(err);
-          });
-          res.status(201).json(item);
-        });
-      }
-    );
+        res.status(201).json(item);
+      });
+    }
   },
 };
